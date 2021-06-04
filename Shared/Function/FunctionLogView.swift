7ec0f6231d9fc1be08f65c96e3252bfe051f970d
Lisin Dmitriy <lisindima1996@gmail.com>
2021-06-04 20:21:52 +0700

    Удалось получить весь список

--- Shared/Function/FunctionLogView.swift
+++ Shared/Function/FunctionLogView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct FunctionLogView: View {
     @StateObject private var webSocket = WebSocket()
+    @State private var test = [
+        FunctionLog(level: "", message: "", requestId: "", ts: 1622811287877.0, type: "start"),
+        FunctionLog(level: Optional("INFO"), message: Optional("sent: 1\n"), requestId: Optional("de684c79"), ts: 1622811288061.0, type: "line"),
+        FunctionLog(level: Optional("INFO"), message: Optional("failed: 0\n"), requestId: Optional("de684c79"), ts: 1622811288061.0, type: "line"),
+        FunctionLog(level: Optional("INFO"), message: Optional("error: []\n"), requestId: Optional("de684c79"), ts: 1622811288061.0, type: "line"),
+        FunctionLog(level: Optional("INFO"), message: Optional("Duration: 182.47 ms\tMemory Usage: 80 MB\t"), requestId: Optional("de684c79"), ts: 1622811288063.0, type: "report"),
+        FunctionLog(level: Optional(""), message: Optional(""), requestId: Optional(""), ts: 1622811288371.0, type: "start"),
+        FunctionLog(level: Optional("INFO"), message: Optional("sent: 1\n"), requestId: Optional("023e0cd2"), ts: 1622811288561.0, type: "line")
+    ]
     
     var body: some View {
-        List {
-            ForEach(webSocket.functionLog, id: \.self) { log in
-                Text("\(log.ts)")
+        ScrollView([.horizontal, .vertical]) {
+            VStack(alignment: .leading) {
+                ForEach(test, id: \.id, content: FunctionLogItems.init)
             }
         }
         .navigationTitle("Function log")
@@ -22,12 +31,47 @@ struct FunctionLogView: View {
     }
 }
 
-struct FunctionLog: Codable, Hashable {
+struct FunctionLogItems: View {
+    let log: FunctionLog
+    
+    var body: some View {
+        if log.type != "start" {
+            HStack {
+                Text(log.date.logDate)
+                if let requestId = log.requestId {
+                    Text(requestId)
+                }
+                if let level = log.level {
+                    Text(level)
+                }
+                if let message = log.message {
+                    Text(message)
+                }
+            }
+            .font(.system(.caption2, design: .monospaced))
+        }
+    }
+}
+
+struct FunctionLog: Codable, Identifiable {
+    let id = UUID()
     let level: String?
     let message: String?
     let requestId: String?
-    let ts: Int
+    let ts: TimeInterval
     let type: String
+    
+    var date: Date {
+        Date(timeIntervalSince1970: ts)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case level
+        case message
+        case requestId = "request_id"
+        case ts
+        case type
+    }
 }
 
 struct WebSocketAuth: Codable {
@@ -42,27 +86,36 @@ class WebSocket: ObservableObject {
     
     private var webSocketTask: URLSessionWebSocketTask?
     
+    let auth = WebSocketAuth(
+        accessToken: "_8KwmZ6rPGi-gio0EeSweXuQNNa5y8R1rLeG2uOeWjM",
+        accountId: "008585217041",
+        functionId: "28cebd9df8f2856f91e7b7725f9555bcd454de3125d0c573a4590ec11ab9d201",
+        siteId: "293ac253-ed75-48c6-8cbb-c5488fbb720c"
+    )
+    
     func connect() {
-        guard webSocketTask == nil else {
-            return
+        if webSocketTask == nil {
+            disconnect()
         }
         
         let url = URL(string: "wss://socketeer.services.netlify.com/function/logs")!
         
         webSocketTask = URLSession.shared.webSocketTask(with: url)
-        webSocketTask?.receive(completionHandler: onReceive)
         webSocketTask?.resume()
         
+        sendMessage()
+        receiveMessage()
+    }
+    
+    func disconnect() {
+        webSocketTask?.cancel(with: .goingAway, reason: nil)
+        functionLog.removeAll()
+    }
+    
+    private func sendMessage() {
         let encoder = JSONEncoder()
         encoder.keyEncodingStrategy = .convertToSnakeCase
         encoder.outputFormatting = .sortedKeys
-        
-        let auth = WebSocketAuth(
-            accessToken: "_8KwmZ6rPGi-gio0EeSweXuQNNa5y8R1rLeG2uOeWjM",
-            accountId: "008585217041",
-            functionId: "28cebd9df8f2856f91e7b7725f9555bcd454de3125d0c573a4590ec11ab9d201",
-            siteId: "293ac253-ed75-48c6-8cbb-c5488fbb720c"
-        )
         
         guard let data = try? encoder.encode(auth) else { return }
         guard let jsonString = String(bytes: data, encoding: .utf8) else { return }
@@ -76,27 +129,27 @@ class WebSocket: ObservableObject {
         }
     }
     
-    func disconnect() {
-        webSocketTask?.cancel(with: .normalClosure, reason: nil)
-        functionLog.removeAll()
-    }
-
-    private func onReceive(incoming: Result<URLSessionWebSocketTask.Message, Error>) {
-        if case let .success(value) = incoming {
-            print(value)
-            if case let .string(text) = value {
-                guard let data = text.data(using: .utf8),
-                      let log = try? JSONDecoder().decode(FunctionLog.self, from: data)
-                else {
-                    return
+    private func receiveMessage() {
+        webSocketTask?.receive {[weak self] result in
+            switch result {
+            case .failure(let error):
+                print("Error in receiving message: \(error)")
+            case .success(.string(let str)):
+                do {
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode(FunctionLog.self, from: Data(str.utf8))
+                    DispatchQueue.main.async{
+                        self?.functionLog.append(result)
+                        print(result)
+                    }
+                } catch {
+                    print("error is \(error.localizedDescription)")
                 }
+                self?.receiveMessage()
                 
-                DispatchQueue.main.async {
-                    self.functionLog.append(log)
-                }
+            default:
+                print("default")
             }
-        } else if case let .failure(error) = incoming {
-            print("Error", error)
         }
     }
 }
